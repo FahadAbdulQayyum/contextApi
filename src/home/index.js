92e8b0fa6477fb02052a398d32cce822b10f8eff
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-12-24 01:23:10 +0500

    added key=v.id

--- src/home/index.js
+++ src/home/index.js
@@ -4,37 +4,18 @@ import Products from '../../products.json'
 const Index = () => {
 
     const [prod, setProd] = useState({})
-    // const [prod, setProd] = useState()
 
     const addBtn = v => {
         const prd = { ...Products, quantity: v.quantity++ }
-        // setProd({ ...prd })
         setProd(prd)
-        // console.log('vv', prd)
     }
 
     return (
         <div
             className='bg-orange-400 text-white'
         >
-            <h1>AsSalam-O-Alaikum</h1>
-            {/* <div
-                className='flex bg-violet-400'
-            >
-                <p>Onion</p>
-                <button
-                    className='bg-slate-500 py-1 px-2 ml-5'
-                >Add to Cart</button>
-            </div>
-            <div
-                className='flex bg-red-500'
-            >
-                <p>Tomato</p>
-                <button
-                    className='bg-slate-500 py-1 px-2 ml-5'
-                >Add to Cart</button>
-            </div> */}
             {Products.map(v => <div
+                key={v.id}
                 className='flex py-1'
             >
                 <p>{v.name}</p>
@@ -42,14 +23,9 @@ const Index = () => {
                     className='bg-slate-500 py-1 px-2 ml-5 hover:scale-105 transition-transform'
                     onClick={() => addBtn(v)}
                 >Add to Cart - {v.quantity}</button>
-                {/* // >Add to Cart - {console.log('prod.quantity', prod.quantity)}</button> */}
-                {/* >Add to Cart - {prod.quantity}</button> */}
-                {/* // >Add to Cart - {prd[v.id]?.quantity}</button> */}
-                {/* // >Add to Cart - {console.log('prod.quantity', prod)}</button> */}
-                {/* >Add to Cart - {console.log('prod.quantity', prod)}</button> */}
             </div>)
             }
-        </div >
+        </div>
     )
 }
 
