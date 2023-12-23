143f2dedeaa40b63783543856c8dab55c13aaf71
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-12-24 00:42:09 +0500

    navbar made and tailwind setup made successful

---
+++ src/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div
+            className='bg-orange-400 text-white'
+        >
+            <h1>AsSalam-O-Alaikum</h1>
+        </div>
+    )
+}
+
+export default Index
