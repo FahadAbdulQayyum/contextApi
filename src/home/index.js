877b400d0155e90189193043cc0c4cfe1dbd507a
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-12-24 01:00:27 +0500

    products.json added and 'add to cart' with quantity also introduced with button clicked quantity incrementation

--- src/home/index.js
+++ src/home/index.js
@@ -1,12 +1,45 @@
 import React from 'react'
+import Products from '../../products.json'
 
 const Index = () => {
+
+    const addBtn = v => {
+        const prd = { ...Products, quantity: v.quantity++ }
+        console.log('vv', v)
+    }
+
     return (
         <div
             className='bg-orange-400 text-white'
         >
             <h1>AsSalam-O-Alaikum</h1>
-        </div>
+            {/* <div
+                className='flex bg-violet-400'
+            >
+                <p>Onion</p>
+                <button
+                    className='bg-slate-500 py-1 px-2 ml-5'
+                >Add to Cart</button>
+            </div>
+            <div
+                className='flex bg-red-500'
+            >
+                <p>Tomato</p>
+                <button
+                    className='bg-slate-500 py-1 px-2 ml-5'
+                >Add to Cart</button>
+            </div> */}
+            {Products.map(v => <div
+                className='flex py-1'
+            >
+                <p>{v.name}</p>
+                <button
+                    className='bg-slate-500 py-1 px-2 ml-5 hover:scale-105 transition-transform'
+                    onClick={() => addBtn(v)}
+                >Add to Cart</button>
+            </div>)
+            }
+        </div >
     )
 }
 
