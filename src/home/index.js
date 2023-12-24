4dda47eae76d755aaad157230111e1bb02d56da5
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-12-24 19:11:20 +0500

    Now successfully system store the data in localstorage of selected items

--- src/home/index.js
+++ src/home/index.js
@@ -3,12 +3,34 @@ import Products from '../../products.json'
 
 const Index = () => {
 
-    const [prod, setProd] = useState({})
+    // const [prod, setProd] = useState({})
+    // const [prod, setProd] = useState([...Products])
+    const [prod, setProd] = useState()
+
+    // let prd = JSON.parse(localStorage.getItem('addToCart'))
+    // let prdd = Products
+
+    // let prdd = prd ? JSON.parse(localStorage.getItem('addToCart')) : prdd
+
 
     useEffect(() => {
-        const prd = JSON.parse(localStorage.getItem('addToCart'))
-        console.log('prddd', prd)
-        setProd(prd)
+        let prd = JSON.parse(localStorage.getItem('addToCart'))
+        // console.log('prddd', [prd].filter(v => console.log('vv', v)))
+        console.log('prddd', prd?.filter(v => console.log('vv', v)))
+        // console.log('prddd-', [prd])
+        console.log('prddd-', prd)
+
+        // prdd = prd ? JSON.parse(localStorage.getItem('addToCart')) : prdd
+
+        // setProd(prd ? JSON.parse(localStorage.getItem('addToCart')) : prdd)
+        setProd(prd ? JSON.parse(localStorage.getItem('addToCart')) : Products)
+
+        // prd ? setProd([prd]) : setProd(Products)
+
+        // prd ? setProd(prd) : setProd(prdd)
+
+        // prd ? setProd(prd) : setProd(Products)
+        // setProd(prd)
     }, [])
 
     const addBtn = v => {
@@ -17,9 +39,20 @@ const Index = () => {
         // const prd = JSON.parse(localStorage.getItem('addToCart'))
         // console.log('prddd', prd)
         // const prd = { ...Products, quantity: v.quantity++ }
-        // setProd(prd)
+
+        // prdd = { ...prdd, quantity: v.quantity++ }
+        // prdd = [...prdd, v.quantity++]
+        // let prdd = [...Products, v.quantity++]
+
+        let prdd = [...prod, v.quantity++]
+        console.log('prodd', prdd)
+        setProd(prdd)
         // localStorage.setItem('addToCart', JSON.parse(prd))
-        localStorage.setItem('addToCart', JSON.stringify(prd))
+        // localStorage.setItem('addToCart', JSON.stringify(prd))
+        // localStorage.setItem('addToCart', JSON.stringify([prdd]))
+        localStorage.setItem('addToCart', JSON.stringify(prdd))
+        // localStorage.setItem('addToCart', prd)
+        // localStorage.setItem('addToCart', JSON.parse(prd))
     }
 
     return (
@@ -27,16 +60,20 @@ const Index = () => {
             className='bg-orange-400 text-white'
         >
             {/* {prod && prod?.map(v => <div */}
-            {Products.map(v => <div
+            {/* {prod && prod?.map(v => <div */}
+            {/* {Products.map(v => <div */}
+            {/* {prdd.map(v => <div */}
+            {prod?.map(v => (typeof v === 'object' && <div
                 key={v.id}
                 className='flex py-1'
             >
                 <p>{v.name}</p>
+                {/* <p>{console.log('v.name', typeof v === 'object')}</p> */}
                 <button
                     className='bg-slate-500 py-1 px-2 ml-5 hover:scale-105 transition-transform'
                     onClick={() => addBtn(v)}
-                >Add to Cart - {v.quantity}</button>
-            </div>)
+                >Add to Cart - {v?.quantity}</button>
+            </div>))
             }
         </div >
     )
